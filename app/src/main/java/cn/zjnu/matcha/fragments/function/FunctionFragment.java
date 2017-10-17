defba2f746df3886317a496879b1523b08498c06
marsor <369135912@qq.com>
2017-10-17 16:31:41 +0800

    功能Fragment

---
+++ app/src/main/java/cn/zjnu/matcha/fragments/function/FunctionFragment.java
@@ -0,0 +1,19 @@
+package cn.zjnu.matcha.fragments.function;
+
+
+import android.support.v4.app.Fragment;
+
+import cn.zjnu.matcha.R;
+import cn.zjnu.matcha.core.app.BaseFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FunctionFragment extends BaseFragment {
+
+    @Override
+    protected Object getContentLayoutId() {
+        return R.layout.fragment_function;
+    }
+
+}

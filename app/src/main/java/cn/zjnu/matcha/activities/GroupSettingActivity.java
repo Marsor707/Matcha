4adeae702ce0bb1766aab10792ca32bd8ab3acf2
fsh <2379001315@qq.com>
2017-10-27 16:00:01 +0800

    无法显示二维码

--- app/src/main/java/cn/zjnu/matcha/activities/GroupSettingActivity.java
+++ app/src/main/java/cn/zjnu/matcha/activities/GroupSettingActivity.java
@@ -1,13 +1,17 @@
 package cn.zjnu.matcha.activities;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import butterknife.BindView;
+import butterknife.OnClick;
+import cn.jpush.im.android.api.model.Conversation;
 import cn.zjnu.matcha.R;
 import cn.zjnu.matcha.core.app.BaseActivity;
 
@@ -21,8 +25,11 @@ public class GroupSettingActivity extends BaseActivity {
     Toolbar mToolbar;
     @BindView(R.id.linear_group_member)
     LinearLayout mLinear;
-    @BindView(R.id.ib_add_group_member)
-    ImageButton mAdd;
+    @BindView(R.id.iv_qrcode)
+    ImageView mQrcode;
+    private final AlertDialog DIALOG = null;
+    private Conversation mConversation;
+    private long mGroupId;
 
     @Override
     protected Object getContentLayoutId() {
@@ -39,13 +46,25 @@ public class GroupSettingActivity extends BaseActivity {
                 finish();
             }
         });
-        mAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GroupMemberActivity.show(v.getContext());
-            }
-        });
     }
+
+    @OnClick(R.id.toolbar)
+    void toQRCode() {
+        final Dialog dialog = new AlertDialog.Builder(this).create();
+        dialog.show();
+        dialog.getWindow().setContentView(R.layout.dialog_qrcode);
+//        GroupInfo groupInfo = (GroupInfo) mConversation.getTargetInfo();
+//        mGroupId = groupInfo.getGroupID();
+//        Bitmap qrcode = QRCode.createQRCode(String.valueOf(mGroupId));
+//        final Window window = DIALOG.getWindow();
+//        if (window != null) {
+//            window.setContentView(R.layout.dialog_qrcode);
+//            window.setWindowAnimations(R.style.anim_panel_up_from_bottom);
+//            window.findViewById(R.id.iv_qrcode).(qrcode);
+//        }
+//        DIALOG.show();
+    }
+
 
     public static void show(Context context) {
         Intent intent = new Intent(context, GroupSettingActivity.class);
